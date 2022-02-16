9f98a0e968dc831d4448eeb74d0f7e4270e49f49
Rafael Reis <rafa.araujoreis@gmail.com>
2022-02-16 18:35:03 -0300

    styles header application

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,16 @@
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
+import { SearchButton } from '../SearchButton';
 
 export function Header() {
   return (
     <header className={styles.headerContainer}>
       <div className={styles.hero}>
         <img src="./images/logo.svg" alt="Logo Brasil Paralelo" />
-        <p>Entretenimento e Educação</p>
+        <div className={styles.search}>
+          <p>Entretenimento e Educação</p>
+          <SearchButton />
+        </div>
         <SignInButton />
       </div>
     </header>
