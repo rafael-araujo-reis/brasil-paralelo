71a92ba9e504520a1543bf70a28f1b7063cb25c2
Rafael Reis <rafa.araujoreis@gmail.com>
2022-02-16 18:34:13 -0300

    create component search

---
+++ src/components/SearchButton/index.tsx
@@ -0,0 +1,8 @@
+import { FiSearch } from 'react-icons/fi';
+import styles from './styles.module.scss';
+
+export function SearchButton() {
+  return (
+    <FiSearch className={styles.iconSearch} />
+  );
+}
