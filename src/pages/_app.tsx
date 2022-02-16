67d08d94ce247c19cf664e9b894e4b39a3a59e64
Rafael Reis <rafa.araujoreis@gmail.com>
2022-02-16 09:12:28 -0300

    import footer and style

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import { AppProps } from "next/app";
+import { Footer } from "../components/Footer";
 import { Header } from "../components/Header";
+import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
+
   return (
     <>
       <Header />
       <Component {...pageProps} />
+      <Footer />
     </>
   );
 }
