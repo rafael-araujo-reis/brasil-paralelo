9086066d3904ae7efbef3669d98fb0521e552220
Rafael Reis <rafa.araujoreis@gmail.com>
2022-02-22 08:33:19 -0300

    create page login

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -1,7 +1,35 @@
+import { SignInButton } from '../../components/SignInButton';
+import { SocialMediaButton } from '../../components/SocialMediaButton';
 import styles from './styles.module.scss';
 
 export default function SignIn() {
   return (
-    <p>Página do login</p>
+    <div className={styles.containerSign}>
+
+      <div className={styles.backgroundDesktop}></div>
+
+      <div className={styles.containerLogin}>
+        <img src="images/logo.svg" alt="Logo Brasil Pararelo" />
+
+        <div className={styles.loginSocialMedia}>
+          <p>Acessar com redes sociais</p>
+          <SocialMediaButton nameSocialMedia={'Google'} />
+          <SocialMediaButton nameSocialMedia={'Facebook'} />
+          <SocialMediaButton nameSocialMedia={'Twitter'} />
+        </div>
+
+        <p>ou</p>
+
+        <div className={styles.loginEmail}>
+          <p>Acessar com e-mail e senha</p>
+          <input type="text" placeholder='Informe seu e-mail' />
+          <input type="password" placeholder='Digite sua senha' />
+          <button>Acessar conta</button>
+          <button>Esqueci minha senha</button>
+        </div>
+
+      </div>
+
+    </div>
   );
 }
